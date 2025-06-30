882b9ac6e5db29d2ed4a03420861a7e1e71bf9e0
jwt <741358192@qq.com>
2025-06-30 21:32:34 +0800

    feat(steamworks): 添加 SFriends 组件的聊天和状态更新处理
    - 在 SFriends 组件中添加了多个 SteamFriends 事件的处理
    - 实现了聊天消息、群组聊天消息、好友状态更新等事件的日志记录
    - 添加了对游戏状态加入请求、游戏服务器改变请求等事件的处理
    - 设置 ListenForFriendsMessages为 true 以接收好友消息

--- GodotTools/addons/steamworks/src/component/SFriends.cs
+++ GodotTools/addons/steamworks/src/component/SFriends.cs
@@ -16,12 +16,16 @@ public partial class SFriends : SteamComponent
     public override void _Ready()
     {
         base._Ready();
-        SteamFriends.OnChatMessage += (friend, s1, s2) => { };
-        SteamFriends.OnClanChatMessage += (friend, s1, s2) => { };
-        SteamFriends.OnFriendRichPresenceUpdate += (friend) => { };
-        SteamFriends.OnGameLobbyJoinRequested += (lobby, steamId) => { };
-        SteamFriends.OnGameOverlayActivated += (result) => { };
-        // TODO
+        SteamFriends.OnChatMessage += (friend, s1, s2) => { Log.Info($"steam 收到聊天消息 {friend} {s1} {s2}"); };
+        SteamFriends.OnClanChatMessage += (friend, s1, s2) => { Log.Info($"steam 群组收到聊天消息 {friend} {s1} {s2}"); };
+        SteamFriends.OnFriendRichPresenceUpdate += (friend) => { Log.Info($"steam 好友状态更新 {friend}"); };
+        SteamFriends.OnGameLobbyJoinRequested += (lobby, steamId) => { Log.Info($"steam 群组加入请求 {lobby} {steamId}"); };
+        SteamFriends.OnGameOverlayActivated += (result) => { Log.Info($"steam 覆盖界面界面 {(result ? "激活" : "关闭")}"); };
+        SteamFriends.OnGameRichPresenceJoinRequested += (friend, s) => { Log.Info($"steam 游戏状态加入请求 {friend} {s}"); };
+        SteamFriends.OnGameServerChangeRequested += (s1, s2) => { Log.Info($"steam 游戏服务器改变请求 {s1} {s2}"); };
+        SteamFriends.OnOverlayBrowserProtocol += (s) => { Log.Info($"steam 覆盖界面浏览器协议 {s}"); };
+        SteamFriends.OnPersonaStateChange += (friend) => { Log.Info($"steam 好友状态改变 {friend}"); };
+        SteamFriends.ListenForFriendsMessages = true;
     }
 
 
